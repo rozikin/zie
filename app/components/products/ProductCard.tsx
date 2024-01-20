eb9585829cf1d4ea4401d6f1fba111e622e47b24
rozikin <khoirurrozikin86@gmail.com>
2024-01-20 22:07:58 +0700

    feat:init first up

---
+++ app/components/products/ProductCard.tsx
@@ -0,0 +1,50 @@
+'use client'
+import { formatPrice } from '@/utils/formatPrice'
+import { truncateText } from '@/utils/truncateText'
+import React from 'react'
+import { Rating } from '@mui/material'
+
+interface ProductCardProps{
+    data: any
+}
+
+const ProductCard:React.FC<ProductCardProps> = ({data}) => {
+    return (
+        <div className='
+        col-span-1 
+        cursor-pointer 
+        border-[1.2px]
+        border-slate-200
+        rounded-sm
+        p-2
+        transition
+        hover:scale-105
+        text-center
+        text-sm
+
+        '>
+            <div className='
+            flex
+            flex-col
+            items-center
+            w-full
+            gap-1
+            '>
+                <div className='aspect-square overflow-hidden relative w-full'>
+                    <img className='w-full h-full object-contain' src={data.images[0].image} alt={data.name} />
+                </div>
+                <div className='mt-4'>
+                    {truncateText(data.name)}
+                </div>
+                <div>
+                    <Rating value={4} readOnly/>
+                </div>
+                <div>{data.reviews.length} reviews</div>
+                <div className='font-semibold'>{formatPrice(data.price)}</div>
+
+            </div>
+        </div>
+    )
+}
+
+export default ProductCard
